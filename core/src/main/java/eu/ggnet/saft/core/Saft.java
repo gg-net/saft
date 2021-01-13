6f1c772983bbfd9c63e5810e9219e78433569cdf
Oliver Günther <oliver.guenther@gg-net.de>
2021-01-13 16:03:46 +0100

    [DWPRO-23] Beim DW versaften einen Bug gefunden und behoben

--- core/src/main/java/eu/ggnet/saft/core/Saft.java
+++ core/src/main/java/eu/ggnet/saft/core/Saft.java
@@ -20,7 +20,8 @@ import java.awt.Component;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.function.*;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -457,7 +458,7 @@ public class Saft {
      * @param parent an optional uielement enclosed by the window which should be the parent, if null main is used.
      * @return the BiFunction.
      */
-    public <Z> BiFunction<Z, Throwable, Z> handler(Optional<UiParent> parent) {
+    public <Z> AndFinallyHandler handler(Optional<UiParent> parent) {
         return new AndFinallyHandler<>(this, parent);
     }
 
@@ -469,7 +470,7 @@ public class Saft {
      * @param <Z> type of incomming value.
      * @return the BiFunction.
      */
-    public <Z> BiFunction<Z, Throwable, Z> handler() {
+    public <Z> AndFinallyHandler handler() {
         return handler(Optional.empty());
     }
 
@@ -482,7 +483,7 @@ public class Saft {
      * @param parent an optional uielement enclosed by the window which should be the parent, if null main is used.
      * @return the BiFunction.
      */
-    public <Z> BiFunction<Z, Throwable, Z> handler(Node parent) {
+    public <Z> AndFinallyHandler handler(Node parent) {
         return handler(Optional.of(UiParent.of(parent)));
     }
 
@@ -495,7 +496,7 @@ public class Saft {
      * @param parent an optional uielement enclosed by the window which should be the parent, if null main is used.
      * @return the BiFunction.
      */
-    public <Z> BiFunction<Z, Throwable, Z> handler(Component parent) {
+    public <Z> AndFinallyHandler handler(Component parent) {
         return handler(Optional.of(UiParent.of(parent)));
     }
 
